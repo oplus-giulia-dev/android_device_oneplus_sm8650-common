b3c32e20c4028fd5d13a7b58d0652a93a79ef25e
madmax7896 <madmax78968@gmail.com>
2025-06-14 02:09:46 +0530

    sm8650-common: init: Mount camera blobs and fastchg fw for giulia based on prjname
    * Ace 5 and 13R have some camera and battery differences.
    * Thanks to @Chandu_078 for the idea.

--- common.mk
+++ common.mk
@@ -220,6 +220,7 @@ PRODUCT_PACKAGES += \
     init.kernel.post_boot-pineapple.sh \
     init.kernel.post_boot-pineapple_default_2_3_2_1.sh \
     init.oplus.rc \
+    init.overlayfs.rc \
     init.kernel.post_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.early_boot.sh \
