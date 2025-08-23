62493a281f3e6d14d1744cd0af2feba265176079
pjgowtham <pjgowtham@gmail.com>
2025-08-23 23:20:51 +0530

    sm8650-common: Remove oplus performance HAL
    Change-Id: Idbd3c234421deb9cc4cb33f7aa0ced5b1bd3a859

--- extract-files.py
+++ extract-files.py
@@ -76,9 +76,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.secure_element-V1-ndk.so', 'android.hardware.secure_element-V1-ndk_odm.so'),
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
         .add_needed('libshims_aidl_fingerprint_v3.oplus.so'),
-    'odm/bin/hw/vendor-oplus-hardware-performance-V1-service': blob_fixup()
-        .add_needed('libbase_shim.so')
-        .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/libcwb_qcom_aidl.so': blob_fixup()
         .add_needed('libui_shim.so'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
